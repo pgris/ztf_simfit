cba51d7900cd3394e6e362f8ff0b4fb9077c6ae2
Philippe Gris <gris@clermont.in2p3.fr>
2022-04-27 16:19:32 +0200

    code modifs to account for new simulations

--- ztf_simfit/ztf_simu.py
+++ ztf_simfit/ztf_simu.py
@@ -7,15 +7,19 @@ import os
 class Simul_lc:
     "Definition of a class that simule light curve"
 
-    def __init__(self, ztfdataDir, logDir, dustmapDir, rcidFile, csvFile, ztf_fields, z_range=(0.01, 0.1), dec_range=(-30, 90), n_det=1,
+    def __init__(self, ztfdataDir, dustmapDir, rcidFile, ztf_fields, z_range=(0.01, 0.1), n_det=1,
                  ntransient=11, seed=70, threshold=1, **kwargs):
         """
         Parameters
         ----------
         ztfdataDir : str
             directory with ztf data
-        logDir:
-           wdir with cadence logs
+        dustmapDir: str
+          dir with dust maps
+        rcidFile: str
+          rcid files (ZTF internal)
+        ztf_fields: str
+           file of ztf_fields (ZTF internal)
         z_range : (int,int)
             redshift range (default=(0.01, 0.1)).
         dec_range : (int,int)
@@ -30,6 +34,12 @@ class Simul_lc:
             S/N requirement for detection (default=1).
         """
 
+        self.z_range = z_range
+        self.ntransient = ntransient
+        self.seed = seed
+        self.n_det = n_det
+        self.threshold = threshold
+        self.kwargs = kwargs
         self.dustmapDir = dustmapDir
         # grab dustmaps if necessary
         if not os.path.isdir(self.dustmapDir):
@@ -39,44 +49,45 @@ class Simul_lc:
         self.dustmapDir += '/sfd'
 
         rcids = os.path.join(ztfdataDir, rcidFile)
-        logObs = os.path.join(logDir, csvFile)
+        #obsPath = os.path.join(obsDir, obsFile)
         ztf_fields = os.path.join(ztfdataDir, ztf_fields)
 
         self.fields = sst.load_ztf_fields(filename=ztf_fields)
         self.ccds = sst.load_ztf_ccds(
             filename=rcids, num_segs=64)  # it's rcid
 
-        self.obs = pd.read_csv(logObs)
-
-        self.simul = self.simul_lc(
-            z_range, dec_range, ntransient, seed, n_det, threshold, **kwargs)
-
-    def __call__(self):
+    def __call__(self, obs, ra_range, dec_range):
         """
+        Parameters
+        --------------
+        obs: array
+          array of observations
         Return
         ------
         lc : LightcurveCollection
             Collection of simulated light curve, to cheak the firt lc : lc[0]
         """
-        survey = self.simul
+        #survey = self.simul
+        survey = self.simul_lc(obs,  ra_range, dec_range, self.z_range,
+                               self.ntransient, self.seed, self.n_det, self.threshold, **self.kwargs)
+
         lc = survey.get_lightcurves()
 
-        print('light curves', lc.meta)
         return lc
 
-    def simul_lc(self, z_range, dec_range, ntransient, seed, n_det, threshold, **kwargs):
+    def simul_lc(self, obs, ra_range, dec_range, z_range, ntransient, seed, n_det, threshold, **kwargs):
 
-        self.obs['field'] = self.obs['field'].astype('int64')
-        self.obs['time'] = self.obs['time'] - 2400000.5
+        obs['field'] = obs['field'].astype('int64')
+        #obs['time'] = obs['time'] - 2400000.5
 
-        plan = simsurvey.SurveyPlan(time=self.obs['time'], band=self.obs['band'], zp=self.obs['zp'], obs_field=self.obs['field'],
-                                    obs_ccd=self.obs['rcid'], skynoise=self.obs['skynoise'],
+        plan = simsurvey.SurveyPlan(time=obs['time'], band=obs['band'], zp=obs['zp'], obs_field=obs['field'],
+                                    obs_ccd=obs['rcid'], skynoise=obs['skynoise'],
                                     fields={k: v for k, v in self.fields.items(
                                     ) if k in ['ra', 'dec', 'field_id', 'width', 'height']},
                                     ccds=self.ccds)
 
-        mjd_range = (plan.cadence['time'].min() - 30,
-                     plan.cadence['time'].max() + 30)
+        mjd_range = (plan.cadence['time'].min(),
+                     plan.cadence['time'].max())
 
         transientprop = {}
         transientprop['lcsimul_func'] = 'basic'
@@ -90,6 +101,7 @@ class Simul_lc:
             'stretch_sigma')
 
         tr = simsurvey.get_transient_generator(zrange=z_range, transient='Ia', template='salt2',
+                                               ra_range=ra_range,
                                                dec_range=dec_range, mjd_range=mjd_range, sfd98_dir=self.dustmapDir,
                                                ntransient=ntransient, seed=seed, transientprop=transientprop)
 
