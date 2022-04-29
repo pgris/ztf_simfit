83914c64c5e4bb87887e4b20e811576e9daf9ead
Philippe Gris <gris@clermont.in2p3.fr>
2022-04-29 15:52:45 +0200

    add plot to visu LC and refit

---
+++ ztf_simfit_plot/ztf_plot.py
@@ -0,0 +1,29 @@
+from ztf_pipeutils.ztf_hdf5 import Read_LightCurve
+from ztf_simfit.ztf_fit import SN_fit
+import sncosmo
+
+
+class VisuLC:
+    def __init__(self, metaFileInput, metaDirInput):
+
+        meta = Read_LightCurve(file_name=metaFileInput, inputDir=metaDirInput)
+        metaTable = meta.get_table(path='meta')
+
+        metadata = metaTable.meta
+        # get lc
+        lcDir = metadata['directory']
+        lcName = metadata['file_name']
+
+        self.lcs = Read_LightCurve(file_name=lcName, inputDir=lcDir)
+
+        # print SNIDS
+
+        print(metaTable['path'])
+
+    def plot(self, lcpath):
+
+        lc = self.lcs.get_table(lcpath)
+
+        # trying to fit here
+        fit = SN_fit(lc)
+        result, fitted_model = fit('sncosmo', plot=True)
