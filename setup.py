6a100483995b41dd6d1c520a3485e086a15bf27d
Philippe Gris <gris@clermont.in2p3.fr>
2022-04-29 16:03:03 +0200

    add new packages for install: iminuit, ztf_simfit_plot

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setup(
     author='M. Bailleul, Ph. Gris',
     author_email='manon.bailleul@etu.uca.fr,philippe.gris@clermont.in2p3.fr',
     license='BSD',
-    packages=['ztf_simfit', 'ztf_simfit_input', 'ztf_data'],
+    packages=['ztf_simfit', 'ztf_simfit_input', 'ztf_data', 'ztf_simfit_plot'],
     package_data={'ztf_simfit_input': ['*.txt'], 'ztf_data': ['*.txt']},
     python_requires='>=3.5',
     zip_safe=False,
@@ -22,6 +22,7 @@ setup(
         'ztf_pipeutils>=0.1',
         'sncosmo>=1.3.1',
         'simsurvey >=0.1',
-        'dustmaps'
+        'dustmaps',
+        'iminuit'
     ],
 )
